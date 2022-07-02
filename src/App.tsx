5df35c2de2d973a08387f75d8a7e20531f7d679c
Gabriel Oliveira <gabrieldanuzoliveira358@gmail.com>
2022-07-02 01:13:06 -0300

    Finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {ThemeProvider} from 'styled-components';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+
+import Home from './pages/Home';
+import New from './pages/New';
 
 function App() {
+
+  const theme = {
+    primary: "#322153",
+    secondary: "#6C63FF",
+    background: "#F0F0F5",
+    text: "#6C6C80",
+    white: "#FFF",
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path='/new'> 
+          <New />
+        </Route>
+        </Switch>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
